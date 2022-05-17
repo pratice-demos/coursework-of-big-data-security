0b3f0c9011bc6fc1342e29ff110cb5fa678eb3eb
OxalisCu <2127298698@qq.com>
2022-05-17 12:39:19 +0800

    liu 加密算法改 bug

--- 第二次上机/data-safety-retrival/liu.py
+++ 第二次上机/data-safety-retrival/liu.py
@@ -5,18 +5,18 @@ import numpy as np
 class Liu:
     # 初始化密钥
     def __init__(self):
-        self.a = random.randrange(1000, 10000)
-        self.b = random.randrange(1000, 10000)
+        self.a = random.randrange(1, 100)
+        self.b = random.randrange(1, 100)
 
     # 加密
     def enc(self, plain):
-        n = random.randrange(1, self.b)
+        n = random.randrange(1, self.a - 1)
         cypher = self.a * plain + self.b + n
 
         return cypher, n
 
     # 解密
     def dec(self, cypher, n):
-        plain = (cypher - n - self.b) / self.a
+        plain = (cypher - n - self.b) // self.a
 
         return plain
